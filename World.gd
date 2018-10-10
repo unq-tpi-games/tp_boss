95c5c8dc55ea29c60074dc42c7a20dd408a57630
marcia-t <tejedamarcia@gmail.com>
2018-10-10 00:30:44 -0300

    animal resource

--- World.gd
+++ World.gd
@@ -1,14 +1,20 @@
 extends Node
 var tree = preload("res://resources/Trees.tscn")
+var sheep = preload("res://resources/Animal.tscn")
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
 	var new_resource
+	var sheep_resource
 	new_resource = tree.instance()
 	new_resource.position =  Vector2(250, 100)
 	add_child(new_resource)
+	
+	sheep_resource = sheep.instance()
+	sheep_resource.position =  Vector2(400, 450)
+	add_child(sheep_resource)
 	
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
