1ae277c2365846127248d7d31e4733ffedb840b2
marcia-t <tejedamarcia@gmail.com>
2018-11-17 15:31:23 -0300

    using resources, changing nodes

--- Resource.gd
+++ Resource.gd
@@ -2,6 +2,8 @@ extends Node
 
 var available
 const MAX_QUANTITY = 100
+var being_used = false
+var character
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -10,14 +12,25 @@ func _ready():
 
 func use(quant):
 	available = available-quant
+	self.update()
+	
+func update():
+	$Status/available.text = "Disp: "+str(available)
 
 func _process(delta):
-	#Ver cómo desaparecer a todos los hijos de los nodos
 	if available == 0:
 		hide()
 		queue_free()
+	if(being_used):
+		if Input.is_action_pressed("ui_accept"):
+			get_node("/root/World").gather(character,self)
 	pass
 
-func _on_Resource_body_entered(body):
-	if body.name == "Char":
-		get_node("/root/World").gather(body,self)
+func _on_Area2D_body_entered(body):
+	if body.name == "Char" :
+		character = body
+		being_used = true
+		
+func _on_Area2D_body_exited(body):
+	being_used = false
+	pass # replace with function body
