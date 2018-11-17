8f73c0236a4c558c521fdeafb734f06591658ec5
Juan Hualampa <juan.hualampa@gmail.com>
2018-11-17 16:09:57 -0300

    init script

---
+++ Enemies.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# a través del tiempo y en distintas subclases,
+# estas variables se irán modificando para agregar dificultad al juego
+var speed = 100
+var damage = 1
+
+func _ready():
+	#crear fuera de la cámara
+	pass
+
+#moverse hacia el punto del personaje mediante pathfinding & tweening
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
