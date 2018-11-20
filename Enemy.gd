c6e8bbb4946059ee19464d1572cdb80fee63ae22
Juan Hualampa <juan.hualampa@gmail.com>
2018-11-19 22:38:19 -0300

    fixed enemies movement. Check initial pos & collisions

--- Enemy.gd
+++ Enemy.gd
@@ -2,18 +2,31 @@ extends Node2D
 
 var tween
 var obj_pos
-var attack_duration = 10 #default
+var attack_duration = 5 #default
 
-func attack(pos):
-	print(pos)
-	#position = pos
+#posicion inicial en base a lo que muestra la camara
+func calculate_init_pos():
+	camera_x = get_viewport().get_node("/root/World/Camera2D")#get camera limits /2
+	camera_y = get_viewport().get_node("/root/World/Camera2D")#get camera limits /2
+	
+	return Vector2(rand([-camera_x, camera_x]), rand(-camera_y, camera_)) # ???
+
+func move_to(pos):
+	#print(pos)
+	self.set_position(pos)
 
 func _ready():
 	tween = Tween.new()
+	tween.set_position(calculate_init_pos)
 	add_child(tween)
 	obj_pos = obj_pos()
 	
 #func _process(delta):
+	#TODO: la posicion deberia actualizarse con la pos del personaje
 	set_ease(tween, obj_pos)
-	attack(obj_pos)
+	move_to(obj_pos)
 
+func _process(delta):
+	#TODO: usar signals
+	if get_position() == obj_pos:
+		attack()
--- FlyingEnemy.gd
+++ FlyingEnemy.gd
@@ -1,8 +1,15 @@
 extends "res://Enemy.gd"
 
 func set_ease(tween, pos):
-	tween.interpolate_method(self, "attack", self.get_position(), pos, attack_duration, Tween.TRANS_CIRC, Tween.EASE_IN)
+	tween.interpolate_method(self, "attack", self.get_position(), pos, attack_duration, Tween.TRANS_CIRC, Tween.EASE_OUT)
 	tween.start()
 
 func obj_pos():
 	return get_node("/root/World/Char").get_position()
+
+func attack():
+	#explota y causa daño en un radio de 10px
+	pass
+
+func _on_CollisionShape2D_tree_entered():
+	pass # replace with function body
