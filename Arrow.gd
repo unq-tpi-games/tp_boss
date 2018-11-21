bb2df322ba9520920d48b77c78f828fc287e0116
marcia-t <tejedamarcia@gmail.com>
2018-11-21 00:29:37 -0300

    attacking enemies, WIP

---
+++ Arrow.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+var direction = Vector2(0, -1)
+var speed = 300
+
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
--- Tower.gd
+++ Tower.gd
@@ -1,26 +1,67 @@
 extends StaticBody2D
+var attacking = 0
+var fire_range = 200
+var fire_next = 0.0
+var time = 0.0
+var level = 1
+var fire_delta = 1.0/5.0
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+# Degrees per radian
+const DEG_PER_RAD = 57.295779513
+
+const arrow = "res://Arrow.tscn"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	set_physics_process(true)
 	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-func attack(body):
-	pass
-
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("enemies"):
-		attack(body)
-		pass
-	if body.name == "Char" :
-		pass
 	
+	
+func _physics_process(delta):
+	time += delta
+	#if enemy_at_range > 0:
+	fire()
+
+
+
+func fire():
+	if time > fire_next:
+		var target_enemy = choose_target()
+		if target_enemy == null:
+			return
+		var scene = load(arrow)
+		var arr = scene.instance()
+		#bullet.set_position(get_position())
+		arr.direction = (target_enemy.get_global_position() - get_global_position()).normalized()
+#		arr.level = level
+		rotate_turret(arr.direction)
+		add_child(arr)
+		move_child(arr, 0)
+		fire_next = time + fire_delta
+
+func rotate_turret(direction):
+	var rad_angle = atan2(direction.x, direction.y) - atan2(0, -1)
+	var angle = (360 - int(rad_angle * DEG_PER_RAD)) % 360
+	var sprite = get_node("Sprite")
+	var hframes = sprite.get_hframes()
+	var frame = int(round(angle / (360.0/hframes))) % hframes
+	sprite.set_frame(hframes * (level-1 ) + frame)
+
+
+func choose_target():
+	var target = null
+	var pos = get_global_position()
+	for enemy in get_tree().get_nodes_in_group("enemies"):
+		if pos.distance_to(enemy.get_global_position()) <= fire_range:
+			if target == null or enemy.get_global_position().x > target.get_global_position().x:
+				target = enemy
+	return target
+
+
+#func _on_Area2D_body_entered(body):
+#	if body.is_in_group("enemies") && attacking == 0:
+#		attack(body)
+#		attacking+=1
+#		pass
+#	if body.name == "Char" :
+#		pass
+#
