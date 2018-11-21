48801e3e03fa84271e92778c0f4fb210954b7dbc
marcia-t <tejedamarcia@gmail.com>
2018-11-21 11:42:07 -0300

    arrows and enemies colliding

--- FlyingEnemy.gd
+++ FlyingEnemy.gd
@@ -10,3 +10,14 @@ func obj_pos():
 func attack():
 	#explota y causa daño en un radio de 10px
 	pass
+
+func getAttack(body):
+	if body.is_in_group("arrow"):
+		queue_free()
+
+func _on_Area2D_body_entered(body):
+	print("Entro")
+	if body.is_in_group("arrow"):
+		getAttack(body)
+		body.queue_free()
+	pass # replace with function body
