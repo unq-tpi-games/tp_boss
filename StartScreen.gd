4010694bef06b9f8eddba3d29dc0a2c2542b8f73
octi14 <octavio14>
2018-11-23 17:31:50 -0300

    Start screen

---
+++ StartScreen.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _ready():
+	pass
+
+func _on_Button_pressed():
+	get_tree().change_scene("World.tscn")
+	pass # replace with function bodyextends Node
+
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
