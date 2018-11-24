a15d8717d1e3f20612ad08b72777bc6987ad6418
marcia-t <tejedamarcia@gmail.com>
2018-11-24 18:47:53 -0300

    life stats

--- Char.gd
+++ Char.gd
@@ -5,11 +5,11 @@ const WALK_SPEED = 200
 const MAX_HEALTH = 100
 
 var velocity = Vector2()
-var health = 20
+var health = 100
 
-var wood = 300
-var food = 0
-var stone = 300
+var wood = 50
+var food = 50
+var stone = 50
 
 var health_secs = 0
 var food_secs = 0
